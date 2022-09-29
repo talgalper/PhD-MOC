9ab9f73f857c814d36a1a2e89433f39595dd18fe
talgalper <86214510+talgalper@users.noreply.github.com>
2022-09-29 17:28:25 +1000

    Update pcsf_master.R

--- PPI Network Tools/PCSF/pcsf_master.R
+++ PPI Network Tools/PCSF/pcsf_master.R
@@ -32,9 +32,36 @@ plot.PCSF(subnet)
 
 # enrichment analysis not working yet
 library(topGO)
+
+subnet <- PCSF_rand(ppi, snpNOBEN, n = 10, r = 0.1, w = 2, b = 1, mu = 5e-04)
+
 gene_universe <- V(ppi)$name
 res <- enrichment_analysis(subnet, mode=1, gene_universe)
 
+res <- enrichment_analysis(subnet)
 
+plot(res$subent, edge_width = 8, node_size = 30, node_label_cex = 1)
+
+
+### error ###
+'''> res <- enrichment_analysis(subent, mode=1, gene_universe)
+  Performing enrichment analysis...
+
+  Enrichment is being performed by topGO package ...
+
+Building most specific GOs .....
+	( 0 GO terms found. )
+
+Build GO DAG topology ..........
+	( 0 GO terms and 0 relations. )
+Nothing to do:
+Error in split.default(names(sort(nl)), f.index) : 
+  first argument must be a vector
+In addition: Warning messages:
+1: In cluster_edge_betweenness(subnet) :
+  At core/community/edge_betweenness.c:486 : Membership vector will be selected based on the highest modularity score.
+2: In cluster_edge_betweenness(subnet) :
+  At core/community/edge_betweenness.c:493 : Modularity calculation with weighted edge betweenness community detection might not make sense -- 
+  modularity treats edge weights as similarities while edge betwenness treats them as distances.'''
 
 
