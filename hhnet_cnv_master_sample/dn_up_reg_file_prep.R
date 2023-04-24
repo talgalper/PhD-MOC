087b755eb850fe06745c40b2dfff5d9917289eac
talgalper <86214510+talgalper@users.noreply.github.com>
2023-04-24 17:52:46 +1000

    added temporary sample folder

---
+++ hhnet_cnv_master_sample/dn_up_reg_file_prep.R
@@ -0,0 +1,116 @@
+### creates all necessary files for hierarchical hotnet ###
+
+# check BiocManager
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+# required packages
+required_packages <- c("biomaRt", "tidyr", "readr")
+
+# Check if the required packages are installed, if not then install them
+for (package in required_packages) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    BiocManager::install(package)
+    library(package, character.only = TRUE)
+  } else {
+    library(package, character.only = TRUE)
+  }
+}
+
+### create protein (ensembl) to score file
+
+# load ensembl
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+# load weighted copy number data
+weighted_data <- read.csv("results/avg_weighted_seg_mean_subset.csv", row.names = 1)
+df <- weighted_data # save to new variable as backup
+
+# create a vector of gene IDs from your data frame
+gene_ids <- unique(df$gene_id)
+
+# get the corresponding protein ensembles for each gene ID
+protein_ens <- getBM(attributes = c("hgnc_symbol", "ensembl_peptide_id"), 
+                     filters = "hgnc_symbol", 
+                     values = gene_ids, 
+                     mart = ensembl)
+
+# merge the protein IDs with the original data frame based on the gene ID
+protein_to_score <- merge(df, protein_ens, by.x = "gene_id", by.y = "hgnc_symbol")
+
+# drop the gene_id column and remove empty rows
+protein_to_score <- subset(protein_to_score, ensembl_peptide_id != "")
+protein_to_score$gene_id <- NULL
+
+protein_to_score <- protein_to_score[, c("ensembl_peptide_id", "avg_seg_mean")]
+
+# seperate + and - values
+positive_df <- protein_to_score[protein_to_score$avg_seg_mean > 0, ]
+negative_df <- protein_to_score[protein_to_score$avg_seg_mean <= 0, ]
+
+# convert all values in negative_df to positive
+negative_df$avg_seg_mean <- abs(negative_df$avg_seg_mean)
+
+
+### create a indexed edge list file for + and - ###
+
+## cytoscape input
+# create list of proteins for cytoscape
+protein_list <- protein_to_score$ensembl_peptide_id
+protein_list <- as.data.frame(protein_list)
+write.table(protein_list, "protein_list.txt", col.names = F, row.names = F, quote = F)
+
+
+## cytoscape output
+string_data <- read.table("STRING network default edge.csv", header = T, sep = ",", stringsAsFactors = F)
+ppi_list <- subset(string_data, select = c("name"))
+ppi_list <- ppi_list %>% 
+  separate(name, sep = " ", into = c("node_1", "del", "node_2"))
+ppi_list <- subset(ppi_list, select = c("node_1", "node_2"))
+ppi_list$node_1 <- gsub(".*.\\.", "", ppi_list$node_1)
+ppi_list$node_2 <- gsub(".*.\\.", "", ppi_list$node_2)
+
+
+## Compare proteins in positive_df and ppi_list, and keep only those identified in both
+upreg_common_proteins <- intersect(positive_df$ensembl_peptide_id, ppi_list$node_1)
+positive_df <- positive_df[positive_df$ensembl_peptide_id %in% upreg_common_proteins, ]
+upreg_ppi_list <- ppi_list[ppi_list$node_1 %in% upreg_common_proteins, ]
+
+# Repeat the above for negative_df
+dnreg_common_proteins <- intersect(negative_df$ensembl_peptide_id, ppi_list$node_1)
+negative_df <- negative_df[negative_df$ensembl_peptide_id %in% dnreg_common_proteins, ]
+dnreg_ppi_list <- ppi_list[ppi_list$node_1 %in% dnreg_common_proteins, ]
+
+
+write_tsv(positive_df, "hierarchical-hotnet_upreg/data/upreg_protein_to_score.tsv", col_names = F)
+write_tsv(negative_df, "hierarchical-hotnet_dnreg/data/dnreg_protein_to_score.tsv", col_names = F)
+
+
+### create protein index file ###
+upreg_index_to_protein <- subset(positive_df, select = c("ensembl_peptide_id"))
+upreg_index_to_protein$row_num <- seq.int(nrow(upreg_index_to_protein))
+upreg_index_to_protein <- upreg_index_to_protein[c("row_num", "ensembl_peptide_id")]
+
+dnreg_index_to_protein <- subset(negative_df, select = c("ensembl_peptide_id"))
+dnreg_index_to_protein$row_num <- seq.int(nrow(dnreg_index_to_protein))
+dnreg_index_to_protein <- dnreg_index_to_protein[c("row_num", "ensembl_peptide_id")]
+
+write_tsv(upreg_index_to_protein, "hierarchical-hotnet_upreg/data/upreg_index_to_protein.tsv", col_names = F)
+write_tsv(dnreg_index_to_protein, "hierarchical-hotnet_dnreg/data/dnreg_index_to_protein.tsv", col_names = F)
+
+
+### create a indexed edge list file ###
+upreg_edge_list_index<- data.frame(from = match(upreg_ppi_list$node_1, upreg_index_to_protein$ensembl_peptide_id),
+                                   to = match(upreg_ppi_list$node_2, upreg_index_to_protein$ensembl_peptide_id))
+upreg_edge_list_index <- na.omit(upreg_edge_list_index)
+write_tsv(upreg_edge_list_index, "hierarchical-hotnet_upreg/data/upreg_edge_list_index.tsv", col_names = F)
+
+
+# create indexed edge list for downregulated proteins
+dnreg_edge_list_index <- data.frame(from = match(dnreg_ppi_list$node_1, dnreg_index_to_protein$ensembl_peptide_id),
+                                    to = match(dnreg_ppi_list$node_2, dnreg_index_to_protein$ensembl_peptide_id))
+dnreg_edge_list_index <- na.omit(dnreg_edge_list_index)
+write_tsv(dnreg_edge_list_index, "hierarchical-hotnet_dnreg/data/dnreg_edge_list_index.tsv", col_names = F)
+
+
+
---
+++ hhnet_cnv_master_sample/hierarchical-hotnet_upreg/hhnet_copy_number_run_upreg.sh
@@ -0,0 +1,158 @@
+#!/usr/bin/env bash
+
+data=$PWD/data
+intermediate=$PWD/intermediate
+results=$PWD/results
+
+num_permutations=100
+
+# This script uses gnu parallel to parallizes some of the scripts.  If gnu
+# parallel is not already installed on your system, then you can install it from
+# https://www.gnu.org/software/parallel/.  You can change the num_cores variable
+# to specify the number of cores for your system.
+
+num_cores=8
+
+# Compile Fortran module.
+#cd ../src
+#f2py -c fortran_module.f95 -m fortran_module > /dev/null
+#cd ..
+
+################################################################################
+#
+#   Prepare data.
+#
+################################################################################
+
+# Create data, intermediate data and results, and results directories.
+mkdir -p $data
+mkdir -p $intermediate
+mkdir -p $results
+
+for network in network_1
+do
+    mkdir -p $intermediate/"$network"
+done
+
+for network in network_1
+do
+    for score in scores_1
+    do
+        mkdir -p $intermediate/"$network"_"$score"
+    done
+done
+
+################################################################################
+#
+#   Construct similarity matrices.
+#
+################################################################################
+
+echo "Construct similarity matrices..."
+
+for network in network_1
+do
+    python src/construct_similarity_matrix.py \
+        -i   $data/upreg_edge_list_index.tsv \
+        -o   $intermediate/similarity_matrix.h5 \
+        -bof $intermediate/beta.txt
+done
+
+################################################################################
+#
+#   Permute data.
+#
+################################################################################
+
+echo "Permuting scores..."
+
+for network in network_1
+do
+    for score in scores_1
+    do
+        cp $data/upreg_protein_to_score.tsv $intermediate/scores/scores_0.tsv
+
+        python src/find_permutation_bins.py \
+            -gsf $intermediate/scores/scores_0.tsv \
+            -igf $data/upreg_index_to_protein.tsv \
+            -elf $data/upreg_edge_list_index.tsv \
+            -ms  1000 \
+            -o   $intermediate/score_bins.tsv
+
+        parallel -u -j $num_cores --bar \
+            python src/permute_scores.py \
+                -i  $intermediate/scores/scores_0.tsv \
+                -bf $intermediate/score_bins.tsv \
+                -s  "$i" \
+                -o  $intermediate/scores/scores_"$i".tsv
+            ::: `seq $num_permutations`
+    done
+done
+
+################################################################################
+#
+#   Construct hierarchies.
+#
+################################################################################
+
+echo "Constructing hierarchies..."
+
+for network in network_1
+do
+    for score in scores_1
+    do
+        parallel -u -j $num_cores --bar \
+            python src/construct_hierarchy.py \
+                -smf  $intermediate/similarity_matrix.h5 \
+                -igf  $data/upreg_index_to_protein.tsv \
+                -gsf  $intermediate/scores/scores_"$i".tsv \
+                -helf $intermediate/scores/hierarchy_upreg_edge_list_index_"$i".tsv \
+                -higf $intermediate/scores/hierarchy_upreg_index_to_protein_"$i".tsv
+            ::: `seq 0 $num_permutations`
+    done
+done
+
+################################################################################
+#
+#   Process hierarchies.
+#
+################################################################################
+
+echo "Processing hierarchies..."
+
+# This example uses -lsb/--lower_size_bound 1 because it is a small toy example
+# with 25 vertices.  Use larger value (default is 10) for larger graphs.
+for network in network_1
+do
+    for score in scores_1
+    do
+        python src/process_hierarchies.py \
+            -oelf $intermediate/scores/hierarchy_upreg_edge_list_index_0.tsv \
+            -oigf $intermediate/scores/hierarchy_upreg_index_to_protein_0.tsv \
+            -pelf $(for i in `seq $num_permutations`; do echo " $intermediate/$scores/hierarchy_upreg_edge_list_index_"$i".tsv "; done) \
+            -pigf $(for i in `seq $num_permutations`; do echo " $intermediate/$scores/hierarchy_upreg_index_to_protein_"$i".tsv "; done) \
+            -lsb  1 \
+            -cf   $results/clusters.tsv \
+            -pl   "Network" "Score" \
+            -pf   $results/sizes.pdf \
+            -nc   $num_cores
+    done
+done
+
+################################################################################
+#
+#   Perform consensus.
+#
+################################################################################
+
+echo "Performing consensus..."
+
+python src/perform_consensus.py \
+    -cf  $results/clusters.tsv \
+    -igf $data/upreg_index_to_protein.tsv \
+    -elf $data/upreg_edge_list_index.tsv \
+    -n   network \
+    -s   scores \
+    -t   2 \
+    -cnf $results/consensus_nodes.tsv \
+    -cef $results/consensus_edges.tsv
