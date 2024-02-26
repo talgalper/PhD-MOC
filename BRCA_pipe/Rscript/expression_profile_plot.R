f6656b051768fa62d76b61f86e5e3634c43338c9
talgalper <86214510+talgalper@users.noreply.github.com>
2024-02-27 10:39:00 +1100

    Update expression_profile_plot.R

--- BRCA_pipe/Rscript/expression_profile_plot.R
+++ BRCA_pipe/Rscript/expression_profile_plot.R
@@ -1,6 +1,8 @@
 library(biomaRt)
 library(ggplot2)
 library(edgeR)
+library(reshape2)
+library(ggbreak)
 options(scipen = 999)
 
 ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
@@ -18,59 +20,92 @@ load("RData/Her2/BRCA_Her2_DE_data.RData")
 load("RData/Basal/BRCA_Basal_DE_data.RData")
 load("RData/Normal/BRCA_Normal_DE_data.RData")
 
-LumA_expression <- LumA_unstranded[rownames(LumA_unstranded) %in% gene_id$ensembl_gene_id, ]
-LumB_expression <- LumB_unstranded[rownames(LumB_unstranded) %in% gene_id$ensembl_gene_id, ]
-Her2_expression <- Her2_unstranded[rownames(Her2_unstranded) %in% gene_id$ensembl_gene_id, ]
-Basal_expression <- Basal_unstranded[rownames(Basal_unstranded) %in% gene_id$ensembl_gene_id, ]
-GTEx_normal_expression <- GTEx_data[rownames(GTEx_data) %in% gene_id$ensembl_gene_id, ]
-TCGA_normal_expression <- TCGA_normal_unstranded[rownames(TCGA_normal_unstranded) %in% gene_id$ensembl_gene_id, ]
+# logCPM on raw data
+LumA_CPM <- as.data.frame(cpm(LumA_unstranded))
+LumB_CPM <- as.data.frame(cpm(LumB_unstranded))
+Her2_CPM <- as.data.frame(cpm(Her2_unstranded))
+Basal_CPM <- as.data.frame(cpm(Basal_unstranded))
+TCGA_normal_CPM <- as.data.frame(cpm(TCGA_normal_unstranded))
+GTEx_normal_CPM <- as.data.frame(cpm(GTEx_data))
 
-expression_df <- data.frame(LumA = apply(LumA_expression, 1, mean),
-                            LumB = apply(LumB_expression, 1, mean),
-                            Her2 = apply(Her2_expression, 1, mean),
-                            Basal = apply(Basal_expression, 1, mean),
-                            normal_GTEx = apply(GTEx_normal_expression, 1, mean),
-                            normal_TCGA = apply(TCGA_normal_expression, 1, mean))
+# subset target genes
+LumA_subset <- LumA_CPM[rownames(LumA_CPM) %in% gene_id$ensembl_gene_id, ]
+LumB_subset <- LumB_CPM[rownames(LumB_CPM) %in% gene_id$ensembl_gene_id, ]
+Her2_subset <- Her2_CPM[rownames(Her2_CPM) %in% gene_id$ensembl_gene_id, ]
+Basal_subset <- Basal_CPM[rownames(Basal_CPM) %in% gene_id$ensembl_gene_id, ]
+GTEx_normal_subset <- GTEx_normal_CPM[rownames(GTEx_normal_CPM) %in% gene_id$ensembl_gene_id, ]
+TCGA_normal_subset <- TCGA_normal_CPM[rownames(TCGA_normal_CPM) %in% gene_id$ensembl_gene_id, ]
 
+# create function for SE
+std.error <- function(x) sd(x)/sqrt(length(x))
+
+# apply function over CPM data
+SE_df <- data.frame(LumA = apply(LumA_subset, 1, std.error),
+                    LumB = apply(LumB_subset, 1, std.error),
+                    Her2 = apply(Her2_subset, 1, std.error),
+                    Basal = apply(Basal_subset, 1, std.error),
+                    normal_GTEx = apply(GTEx_normal_subset, 1, std.error),
+                    normal_TCGA = apply(TCGA_normal_subset, 1, std.error))
+
+# calculate mean of CPM for each gene
+mean_CPM_df <- data.frame(LumA = apply(LumA_subset, 1, mean),
+                            LumB = apply(LumB_subset, 1, mean),
+                            Her2 = apply(Her2_subset, 1, mean),
+                            Basal = apply(Basal_subset, 1, mean),
+                            normal_GTEx = apply(GTEx_normal_subset, 1, mean),
+                            normal_TCGA = apply(TCGA_normal_subset, 1, mean))
+
+# put gene IDs back in rownames
 gene_id <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"), 
                                      filters = "ensembl_gene_id", 
-                                     values = rownames(expression_df), 
+                                     values = rownames(mean_CPM_df), 
                                      mart = ensembl)
-
-rownames(expression_df) <- gene_id$external_gene_name
+rownames(mean_CPM_df) <- gene_id$external_gene_name
+rownames(SE_df) <- gene_id$external_gene_name
 
 
 # Transpose the data for plotting
-expression_df <- t(expression_df)
-expression_df <- as.data.frame(expression_df)
+mean_CPM_df <- t(mean_CPM_df)
+mean_CPM_df <- as.data.frame(mean_CPM_df)
+
+SE_df <- t(SE_df)
+SE_df <- as.data.frame(SE_df)
 
 # Add gene names as a column
-expression_df$Gene <- rownames(expression_df)
+mean_CPM_df$subtype <- rownames(mean_CPM_df)
+SE_df$subtype <- rownames(SE_df)
 
 # Melt the data for plotting
-expression_df <- melt(expression_df, id.vars = "Gene")
+mean_CPM_df <- melt(mean_CPM_df, id.vars = "subtype")
+colnames(mean_CPM_df)[2] <- "gene"
+colnames(mean_CPM_df)[3] <- "mean_cpm"
 
-cpm_expression <- expression_df
-cpm_expression$value <- cpm(expression_df$value, log = T)
-colnames(cpm_expression)[3] <- "value"
 
-ggplot(expression_df, aes(x = variable, y = value, fill = Gene)) +
+SE_df <- melt(SE_df, id.vars = "subtype")
+colnames(SE_df)[2] <- "gene"
+colnames(SE_df)[3] <- "SE"
+
+# combine data
+plot_data <- merge(mean_CPM_df, SE_df)
+
+
+ggplot(plot_data, aes(x = gene, y = mean_cpm, fill = subtype)) +
   geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) +
-  labs(x = "Gene", y = "Expression Level (counts)", fill = "Cancer Type") +
+  geom_errorbar(aes(ymin = mean_cpm - SE, ymax = mean_cpm + SE), position = position_dodge(width = 0.8), width = 0.25) +
+  labs(x = "Gene", y = "Mean Expression Level (CPM)", fill = "Cancer Subtype") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank()) +
-  scale_fill_brewer(palette = "Paired")
+  scale_fill_brewer(palette = "Paired") +
+  scale_y_break(c(1000, 4200))
 
-ggplot(cpm_expression, aes(x = variable, y = value, fill = Gene)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) +
-  labs(x = "Gene", y = "Expression Level (logCPM)", fill = "Cancer Type") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) +
-  scale_fill_brewer(palette = "Paired")
+
+
+
+
+
+
 
 
 
