9c3226d5e1dc9b71eef17676b4e4f7832709c7d1
talgalper <86214510+talgalper@users.noreply.github.com>
2024-03-25 18:32:20 +1100

    Created MOC CNV data files

---
+++ MOC_copy_number/MOC_CNVloss_data.R
@@ -0,0 +1,85 @@
+library(biomaRt)
+
+MOC_CNV_data <- read.csv("../../../../../Desktop/kylie_cnv_pipe/Source data_All CN segments_with IDs.csv")
+sample_info <- read.csv("../../RNA-Seq_pipeline/latest/MOC/data/All survival_CN_Aug18.csv")
+sample_info <- subset(sample_info, select = c("GAMUT_ID", "Grade", "Stage"))
+
+MOC_CNV_data <- merge(MOC_CNV_data, sample_info, by.x = "GAMuT.ID", by.y = "GAMUT_ID")
+
+CNV_loss <- MOC_CNV_data[MOC_CNV_data$Event == c("CN Loss", "Homozygous Copy Loss"), ]
+
+# Function to consolidate stages
+consolidate_stages <- function(stages) {
+  # Define a function to map stages to their consolidated forms
+  map_stage <- function(stage) {
+    if (grepl("^I[A-C]$", stage)) {
+      return("I")
+    } else if (grepl("^II[A-C]$", stage)) {
+        return("II")
+    } else if (grepl("^III[A-C]$", stage)) {
+        return("III")
+    } else if (grepl("^III[c]$", stage)) {
+        return("III")
+    } else {
+      return(stage)
+    }
+  }
+  
+  # Apply the mapping function to each stage
+  consolidated_stages <- sapply(stages, map_stage)
+  
+  return(consolidated_stages)
+}
+
+# add a column for new stage IDs
+CNV_loss$stage <- consolidate_stages(CNV_loss$Stage)
+
+# Replace empty strings with NA in the Stage column
+CNV_loss$stage[CNV_loss$stage == ""] <- NA
+
+# remove any rows with no gene terms
+CNV_loss <- CNV_loss[CNV_loss$Gene.Symbols != "", ]
+
+# Split merged gene symbols
+split_genes <- strsplit(CNV_loss$Gene.Symbols, ", ")
+
+# Create a list to store expanded data
+expanded_data <- list()
+
+# Expand the data by duplicating rows for each gene symbol
+for (i in seq_along(split_genes)) {
+  cat("Row:", i, "/", length(split_genes), "\n")
+  expanded_data <- c(expanded_data, lapply(split_genes[[i]], function(x) {
+    data.frame(
+      Gene.Symbols = x,
+      Stage = CNV_loss$stage[i]
+    )
+  }))
+}
+
+expanded_df <- do.call(rbind, expanded_data)
+
+expanded_df$Stage[is.na(expanded_df$Stage)] <- "NA"
+
+result <- table(expanded_df$Gene.Symbols, expanded_df$Stage)
+
+# Calculate total counts for each gene
+gene_total_counts <- rowSums(result)
+
+# Calculate proportions for each stage
+proportions <- prop.table(result, margin = 1) * 100
+
+# Combine total counts and proportions into a dataframe
+CN_loss_result_df <- data.frame(
+  gene = rownames(proportions),
+  counts = gene_total_counts,
+  perc_stage_I = proportions[, "I"],
+  perc_stage_II = proportions[, "II"],
+  perc_stage_III = proportions[, "III"],
+  perc_stage_IV = proportions[, "IV"],
+  perc_NAs = proportions[, "NA"]
+)
+
+result_df_subset <- CN_loss_result_df[CN_loss_result_df$counts > 10, ]
+
+
