79d229b677df19ae2d6bba91dac6c71dced703ac
talgalper <86214510+talgalper@users.noreply.github.com>
2024-07-24 04:59:38 +0000

    Added chanegs for drugbank targets

--- WGCNA/BRCA/Rscript/DE_subset.R
+++ WGCNA/BRCA/Rscript/DE_subset.R
@@ -59,25 +59,29 @@ DE_results <- DE_analysis(counts_matrix = DE_counts_filt$counts_filt,
 # save to onedrive (macOS only)
 save(DE_results, DE_counts_filt, DE_genes, file = "../../../../OneDrive - RMIT University/PhD/large_git_files/WGCNA/DE_subset_data.RData")
 
+# check for targets using new drugbank targets
+DrugBank_targets <- read.csv("../BRCA_pipe/DrugBank_targets_ENS.csv")
+DrugBank_targets_unique <- na.omit(DrugBank_targets)
+DrugBank_targets_unique <- DrugBank_targets_unique[!duplicated(DrugBank_targets_unique$ensembl_gene_id), ]
+
+# targets in low_expr_genes
+temp <- DrugBank_targets_unique[DrugBank_targets_unique$ensembl_gene_id %in% rownames(DE_counts_filt$low_exp_genes), ]
+temp <- DE_counts_filt$low_exp_genes[rownames(DE_counts_filt$low_exp_genes) %in% DrugBank_targets_unique$ensembl_gene_id, ] # get counts of low_expr_genes
+
 # check to see if drugs are in DE dataset
-OpenTargets <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_unique_drug.csv", row.names = 1)
-OpenTargets_raw <- read_tsv("../BRCA_pipe/OpenTargets_data/breast_carcinoma_known_drugs.tsv")
-OpenTargets_updated <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_updated.csv")
+#OpenTargets <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_unique_drug.csv", row.names = 1)
+#OpenTargets_raw <- read_tsv("../BRCA_pipe/OpenTargets_data/breast_carcinoma_known_drugs.tsv")
+#OpenTargets_updated <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_updated.csv")
+#
+#NIH_targets <- read.table("../BRCA_pipe/NIH_BRCA_approved_drugs.txt", sep = "\t")
+#colnames(NIH_targets)[1] <- "approved_drugs"
+#NIH_targets$approved_drugs <- toupper(NIH_targets$approved_drugs)
+#
+#approved_openTargets <- merge(NIH_targets, OpenTargets_raw, by.x = "approved_drugs", by.y = "Drug Name")
+#approved_openTargets <- approved_openTargets[!duplicated(approved_openTargets$approved_drugs) | !duplicated(approved_openTargets$`Target ID`), ]
 
-NIH_targets <- read.table("../BRCA_pipe/NIH_BRCA_approved_drugs.txt", sep = "\t")
-colnames(NIH_targets)[1] <- "approved_drugs"
-NIH_targets$approved_drugs <- toupper(NIH_targets$approved_drugs)
-
-approved_openTargets <- merge(NIH_targets, OpenTargets_raw, by.x = "approved_drugs", by.y = "Drug Name")
-approved_openTargets <- approved_openTargets[!duplicated(approved_openTargets$approved_drugs) | !duplicated(approved_openTargets$`Target ID`), ]
-
-# number of targets in DE dataset
-#temp <- merge(approved_openTargets, DE_results$dif_exp, by.x = "Target ID", by.y = "gene_id", all.x = T)
-#temp <- temp[!duplicated(temp$`Target Approved Symbol`), ]
-#table(is.na(temp$logFC))
-
-temp <- approved_openTargets[approved_openTargets$`Target ID` %in% DE_results$dif_exp$gene_id, ]
-table(unique(approved_openTargets$`Target Approved Symbol`) %in% unique(temp$`Target Approved Symbol`))
+#temp <- approved_openTargets[approved_openTargets$`Target ID` %in% DE_results$dif_exp$gene_id, ]
+#table(unique(approved_openTargets$`Target Approved Symbol`) %in% unique(temp$`Target Approved Symbol`))
 
 # need to re-filter on only tumour set. Make sure to read in WGCNA_functions.R again
 wgcna_counts_filt <- filter_low_expr(tumour_matrix = all_subtypes,
@@ -91,8 +95,8 @@ tumour_data_norm <- vst_norm(counts_df = wgcna_counts_filt$tumour)
 DE_genes <- DE_results$dif_exp$gene_id
 tumour_DE_subset <- tumour_data_norm[, colnames(tumour_data_norm) %in% DE_genes]
 
-temp <- approved_openTargets[approved_openTargets$`Target ID` %in% colnames(tumour_DE_subset), ]
-table(unique(approved_openTargets$`Target Approved Symbol`) %in% unique(temp$`Target Approved Symbol`))
+temp <- DrugBank_targets_unique[DrugBank_targets_unique$ensembl_gene_id %in% colnames(tumour_DE_subset), ]
+table(unique(DrugBank_targets_unique$drugBank_target) %in% unique(temp$drugBank_target))
 
 # PCA
 PCA_tumour <- plot_PCA(expr_data = tumour_DE_subset,
@@ -120,10 +124,11 @@ save(tumour_bwnet, file = "BRCA/RData/DE_subset/tumour_bwnet.RData")
 # intramodular connectivity
 colours <- labels2colors(tumour_bwnet$colors)
 tumour_kWithin <- intramodularConnectivity.fromExpr(tumour_DE_subset, colours, power = 6)
+save(tumour_kWithin, file = "BRCA/RData/DE_subset/tumour_kWithin.RData")
+
 rownames(tumour_kWithin) <- colnames(tumour_DE_subset)
 tumour_kWithin <- tumour_kWithin[order(-tumour_kWithin$kWithin), ]
 
-save(tumour_kWithin, file = "BRCA/RData/DE_subset/tumour_kWithin.RData")
 
 # get top 10 genes for connectivity for each non-preserved module
 tumour_topGenes = list()
@@ -141,7 +146,7 @@ colnames(tumour_topGenes) <- c("ensembl_id", "module")
 
 temp <- approved_openTargets[approved_openTargets$`Target ID` %in% tumour_topGenes$ensembl_id, ]
 
-temp <- merge(tumour_kWithin, approved_openTargets, by.x = "row.names", by.y = "Target ID")
+temp <- merge(tumour_kWithin, DrugBank_targets_unique, by.x = "row.names", by.y = "ensembl_gene_id")
 temp <- temp[!duplicated(temp$Row.names), ]
 
 
@@ -150,58 +155,37 @@ topHubGenes <- chooseTopHubInEachModule(tumour_DE_subset, colorh = tumour_bwnet$
 
 
 
-
-
-
-
-
 ### analyse each cluster individually ###
 module_gene_mapping <- as.data.frame(tumour_bwnet$colors)
 module_gene_mapping <- rownames_to_column(module_gene_mapping)
 colnames(module_gene_mapping) <- c("gene_id", "module")
 
+# check for OpenTargets
 temp <- approved_openTargets[approved_openTargets$`Target ID` %in% module_gene_mapping$gene_id, ]
 table(unique(approved_openTargets$`Target Approved Symbol`) %in% unique(temp$`Target Approved Symbol`))
 
+# check for targets using new drugbank targets
+DrugBank_targets <- read.csv("../BRCA_pipe/DrugBank_targets_ENS.csv")
+DrugBank_targets_unique <- na.omit(DrugBank_targets)
+DrugBank_targets_unique <- DrugBank_targets_unique[!duplicated(DrugBank_targets_unique$ensembl_gene_id), ]
+
+temp <- DrugBank_targets_unique[DrugBank_targets_unique$ensembl_gene_id %in% module_gene_mapping$gene_id, ]
+
+
 # try converting to external_gene_name first to try and avoid conflict between Ensembl IDs with OpenTargets
 # OpenTargets bloke was smoking meth when mapping ensembl IDs to gene symbols
-OpenTargets <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_unique_drug.csv", row.names = 1)
-OpenTargets_raw <- read_tsv("../BRCA_pipe/OpenTargets_data/breast_carcinoma_known_drugs.tsv")
-OpenTargets_updated <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_updated.csv")
 
-NIH_targets <- read.table("../BRCA_pipe/NIH_BRCA_approved_drugs.txt", sep = "\t")
-colnames(NIH_targets)[1] <- "approved_drugs"
-NIH_targets$approved_drugs <- toupper(NIH_targets$approved_drugs)
+#OpenTargets <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_unique_drug.csv", row.names = 1)
+#OpenTargets_raw <- read_tsv("../BRCA_pipe/OpenTargets_data/breast_carcinoma_known_drugs.tsv")
+#OpenTargets_updated <- read.csv("../BRCA_pipe/OpenTargets_data/OpenTargets_updated.csv")
+#
+#NIH_targets <- read.table("../BRCA_pipe/NIH_BRCA_approved_drugs.txt", sep = "\t")
+#colnames(NIH_targets)[1] <- "approved_drugs"
+#NIH_targets$approved_drugs <- toupper(NIH_targets$approved_drugs)
+#
+#approved_openTargets <- merge(NIH_targets, OpenTargets_raw, by.x = "approved_drugs", by.y = "Drug Name")
+#approved_openTargets <- approved_openTargets[!duplicated(approved_openTargets$approved_drugs) | !duplicated(approved_openTargets$`Target ID`), ]
 
-approved_openTargets <- merge(NIH_targets, OpenTargets_raw, by.x = "approved_drugs", by.y = "Drug Name")
-approved_openTargets <- approved_openTargets[!duplicated(approved_openTargets$approved_drugs) | !duplicated(approved_openTargets$`Target ID`), ]
-
-
-library(biomaRt)
-ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-
-genes_converted <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"), 
-                         filters = "ensembl_gene_id", 
-                         values = module_gene_mapping$gene_id, 
-                         mart = ensembl)
-
-genes_converted <- merge(module_gene_mapping, genes_converted, by.x = "gene_id", by.y = "ensembl_gene_id", all.x = T)
-
-OpenTargets_mapping <- merge(genes_converted, OpenTargets_raw, by.x = "gene_id", by.y = "Target ID", all.x = T)
-OpenTargets_mapping <- subset(OpenTargets_mapping, select = c("gene_id", "Target Approved Symbol", "external_gene_name"))
-
-OpenTargets_mapping$combined <- ifelse(OpenTargets_mapping$external_gene_name == "" | is.na(OpenTargets_mapping$external_gene_name), 
-                                       OpenTargets_mapping$`Target Approved Symbol`, OpenTargets_mapping$external_gene_name)
-OpenTargets_mapping$combined <- ifelse(is.na(OpenTargets_mapping$combined), OpenTargets_mapping$gene_id, OpenTargets_mapping$combined)
-OpenTargets_mapping <- OpenTargets_mapping[!duplicated(OpenTargets_mapping$gene_id) | !duplicated(OpenTargets_mapping$combined), ]
-
-
-approved_openTargets_EGN <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"), 
-                                  filters = "ensembl_gene_id", 
-                                  values = unique(approved_openTargets$`Target ID`), 
-                                  mart = ensembl)
-approved_openTargets_EGN <- merge(approved_openTargets, approved_openTargets_EGN, by.x = "Target ID", by.y = "ensembl_gene_id")
-approved_openTargets_EGN <- approved_openTargets_EGN[!duplicated(approved_openTargets_EGN$`Target ID`) & !duplicated(approved_openTargets_EGN$external_gene_name), ]
 
 
 
@@ -214,32 +198,27 @@ for (colour in unique(tumour_bwnet$colors)) {
   rm(module, colour)
 }
 
-# map to gene symbols
-gene_modules_converted <- list()
-for (module in gene_modules) {
-  module <- merge(module, OpenTargets_mapping, all.x = T)
-  module_genes <- module$combined
-  
-  colour <- unique(module$module)
-  gene_modules_converted[[colour]] <- module_genes
-  
-  rm(colour, module_genes)
-}
+# map to gene symbols. No required for drugbank targets
+#gene_modules_converted <- list()
+#for (module in gene_modules) {
+#  module <- merge(module, DrugBank_targets_unique, by.x = "gene_id", by.y = "ensembl_gene_id", all.x = T)
+#  module_genes <- module$gene_id
+#  
+#  colour <- unique(module$module)
+#  gene_modules_converted[[colour]] <- module_genes
+#  
+#  rm(colour, module_genes, module)
+#}
 
-## NEED TO FIX OPENTARGETS DATASET FIRST ##
 # count the number of targets in each module
 counts <- c()
-for (module in names(gene_modules_converted)) {
-  genes <- gene_modules_converted[[colour]]
-  
-  targets_in_genes <- approved_openTargets[approved_openTargets$`Target Approved Symbol` %in% genes, ]
-  targets_in_ensemblGenes <- approved_openTargets[approved_openTargets$`Target ID` %in% genes, ]
-  targets_in_genes <- rbind(targets_in_genes, targets_in_ensemblGenes)
-  
-  unique_targets <- unique(targets_in_genes$`Target Approved Symbol`)
+for (module in gene_modules) {
+  genes <- module$gene_id
+  targets_in_genes <- DrugBank_targets_unique[DrugBank_targets_unique$ensembl_gene_id %in% genes, ]
+  unique_targets <- unique(targets_in_genes$drugBank_target)
   counts <- append(counts, length(unique_targets))
   
-  rm(genes, unique_targets, targets_in_genes, colour)
+  rm(genes, targets_in_genes, unique_targets)
 }
 
 # does not add up to 23 because some gene ensembles = more than one gene symbol
@@ -284,7 +263,7 @@ end_time <- Sys.time()
 end_time - start_time
 
 
-# plot preserved modules
+# plot preserved modules.
 plot_preserved_modules <- function(modulePreservation_data) {
   plot_data <- data.frame(
     cluster = rownames(modulePreservation_data$preservation$Z$ref.Tumour$inColumnsAlsoPresentIn.Control),
@@ -339,12 +318,18 @@ save(preserved_modules, modulePreservation_plt, file = "BRCA/RData/DE_subset/mod
 plot_data <- modulePreservation_plt$plot_data$plot_data
 non_preserved_modules <- plot_data[plot_data$medianRank.pres > 8 & plot_data$Zsummary.pres < 10, ]
 
-nonPreservedGenes <- common_genes[tumour_common_colours %in% non_preserved_modules$cluster]
+nonPreservedGenes <- names(tumour_bwnet$colors)[tumour_bwnet$colors %in% non_preserved_modules$cluster]
 
+# check for targets using new drugbank targets
+DrugBank_targets <- read.csv("../BRCA_pipe/DrugBank_targets_ENS.csv")
+DrugBank_targets_unique <- na.omit(DrugBank_targets)
+DrugBank_targets_unique <- DrugBank_targets_unique[!duplicated(DrugBank_targets_unique$ensembl_gene_id), ]
+
+temp <- DrugBank_targets_unique[DrugBank_targets_unique$ensembl_gene_id %in% nonPreservedGenes, ]
 
 
 
 
 
 #### PCSF with DE subset and weighted with druggability scores (using new druggability score formulae) ####
-
+## expand DE list in STRING and check if more targets were added
