126a202db888851261eaa2c38fbf7192c9673e1c
talgalper <86214510+talgalper@users.noreply.github.com>
2024-09-02 16:32:59 +1000

    Update plot_drug_freq.R

--- Druggability_analysis/R/plot_drug_freq.R
+++ Druggability_analysis/R/plot_drug_freq.R
@@ -1,7 +1,29 @@
+library(ggplot2)
+
+Fpocket_AF <- read.csv("Fpocket/results_2024.05/fpocket_druggability.csv")
+Fpocket_AF <- Fpocket_AF[order(-Fpocket_AF$druggability), ] # remove duplicate uniprot IDs, keep highest druggability
+Fpocket_AF <- Fpocket_AF[!duplicated(Fpocket_AF$uniprot_id), ]
+Fpocket_SM <- read.csv("Fpocket/SWISSMODEL/fpocket_druggability.csv")
+Fpocket_AF$method <- rep("AF", nrow(Fpocket_AF))
+Fpocket_SM$method <- rep("SM", nrow(Fpocket_SM))
+Fpocket_AF <- subset(Fpocket_AF, select = c("uniprot_id", "druggability", "method"))
+Fpocket_SM <- subset(Fpocket_SM, select = c("uniprot_id", "druggability", "method"))
+Fpocket_data <- rbind(Fpocket_AF, Fpocket_SM)
+
+PocketMiner_AF <- read.csv("PocketMiner/results/pocketminer_results_4.0.csv")
+PocketMiner_AF <- PocketMiner_AF[order(-PocketMiner_AF$max_hit), ] # remove duplicate uniprot IDs, keep highest druggability
+PocketMiner_AF <- PocketMiner_AF[!duplicated(PocketMiner_AF$uniprot_id), ]
+PocketMiner_SM <- read.csv("PocketMiner/results/SWISSMODEL/pocketminer_results.csv")
+PocketMiner_AF$method <- rep("AF", nrow(PocketMiner_AF))
+PocketMiner_SM$method <- rep("SM", nrow(PocketMiner_SM))
+PocketMiner_AF <- PocketMiner_AF[, -1]
+PocketMiner_SM <- PocketMiner_SM[, -1]
+PocketMiner_data <- rbind(PocketMiner_AF, PocketMiner_SM)
+
 
 # plot distribution of druggble vs undruggable proteins
-
 temp <- merge(Fpocket_data, PocketMiner_data, by = "uniprot_id", all = T)
+colnames(temp)[4] <- "CP_score"
 temp$druggability[is.na(temp$druggability)] <- 0
 temp$CP_score[is.na(temp$CP_score)] <- 0
 temp$highest_score <- pmax(temp$druggability, temp$CP_score)
@@ -10,7 +32,6 @@ temp <- temp[!duplicated(temp$uniprot_id), ]
 colnames(temp)[3] <- "Fpocket_method"
 colnames(temp)[6] <- "PocketMiner_method"
 temp <- temp[, -5]
-
 
 # Create the source column
 temp$source <- ifelse(temp$highest_score == temp$druggability, 
