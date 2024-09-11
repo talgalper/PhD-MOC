dda5f4201ce178406ae377dfd3875f44a2972521
talgalper <86214510+talgalper@users.noreply.github.com>
2024-09-11 12:15:04 +1000

    Update plot_drug_freq.R

--- Druggability_analysis/R/plot_drug_freq.R
+++ Druggability_analysis/R/plot_drug_freq.R
@@ -6,8 +6,9 @@ Fpocket_AF <- Fpocket_AF[!duplicated(Fpocket_AF$uniprot_id), ]
 Fpocket_SM <- read.csv("Fpocket/SWISSMODEL/fpocket_druggability.csv")
 Fpocket_AF$method <- rep("AF", nrow(Fpocket_AF))
 Fpocket_SM$method <- rep("SM", nrow(Fpocket_SM))
-Fpocket_AF <- subset(Fpocket_AF, select = c("uniprot_id", "druggability", "method"))
-Fpocket_SM <- subset(Fpocket_SM, select = c("uniprot_id", "druggability", "method"))
+Fpocket_SM$struct_score <- Fpocket_SM$struct_score * 100 # convert to percentage
+Fpocket_AF <- subset(Fpocket_AF, select = c("uniprot_id", "druggability", "struct_score", "method"))
+Fpocket_SM <- subset(Fpocket_SM, select = c("uniprot_id", "druggability", "struct_score", "method"))
 Fpocket_data <- rbind(Fpocket_AF, Fpocket_SM)
 
 PocketMiner_AF <- read.csv("PocketMiner/results/pocketminer_results_4.0.csv")
@@ -21,17 +22,17 @@ PocketMiner_SM <- PocketMiner_SM[, -1]
 PocketMiner_data <- rbind(PocketMiner_AF, PocketMiner_SM)
 
 
-# plot distribution of druggble vs undruggable p\roteins
+# plot distribution of druggble vs undruggable proteins
 temp <- merge(Fpocket_data, PocketMiner_data, by = "uniprot_id", all = T)
-colnames(temp)[4] <- "CP_score"
+colnames(temp)[5] <- "CP_score"
 temp$druggability[is.na(temp$druggability)] <- 0
 temp$CP_score[is.na(temp$CP_score)] <- 0
 temp$highest_score <- pmax(temp$druggability, temp$CP_score)
 temp <- temp[order(-temp$highest_score), ]
 temp <- temp[!duplicated(temp$uniprot_id), ]
-colnames(temp)[3] <- "Fpocket_method"
-colnames(temp)[6] <- "PocketMiner_method"
-temp <- temp[, -5]
+colnames(temp)[4] <- "Fpocket_method"
+colnames(temp)[7] <- "PocketMiner_method"
+temp <- temp[, -6]
 
 # Create the source column
 temp$source <- ifelse(temp$highest_score == temp$druggability, 
@@ -69,12 +70,51 @@ SM_data <- subset(SM_data, select = c("uniprot_id", "druggability", "max_hit"))
 SM_data$highest_score <- pmax(SM_data$druggability, SM_data$max_hit)
 SM_data$source <- ifelse(SM_data$highest_score == SM_data$druggability, "Fpocket", "PocketMiner")
 
-temp <- merge(AF_data, SM_data, by = "uniprot_id")
+temp2 <- merge(AF_data, SM_data, by = "uniprot_id")
+
+D_ND <- temp2$uniprot_id[temp2$highest_score.x >= 0.5 & temp2$highest_score.y < 0.5]
+ND_D <- temp2$uniprot_id[temp2$highest_score.x < 0.5 & temp2$highest_score.y >= 0.5]
+
+unchanged <- temp2$uniprot_id[!temp2$uniprot_id %in% c(D_ND, ND_D)]
+
+library(biomaRt)
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+name_converted <- getBM(attributes = c("external_gene_name", "uniprot_gn_id", "description"), 
+                           filters = "external_gene_name", 
+                           values = FDA_drug_targets, 
+                           mart = ensembl)
 
 
 
-table(duplicated(temp$uniprot_id))
+Fpocket_D_ND <- temp2$uniprot_id[temp2$druggability.x >= 0.5 & temp2$druggability.y < 0.5]
+Fpocket_ND_D <- temp2$uniprot_id[temp2$druggability.x < 0.5 & temp2$druggability.y >= 0.5]
 
+Cryptic_D_ND <- temp2$uniprot_id[temp2$max_hit.x >= 0.5 & temp2$max_hit.y < 0.5]
+Cryptic_ND_D <- temp2$uniprot_id[temp2$max_hit.x < 0.5 & temp2$max_hit.y >= 0.5]
+
+unchanged <- temp2$uniprot_id[!temp2$uniprot_id %in% c(Fpocket_D_ND, Fpocket_ND_D, Cryptic_D_ND, Cryptic_ND_D)]
+unchanged <- temp2$uniprot_id
+
+# higher score given to already druggable structure
+table(temp2$highest_score.x > 0.5 & temp2$highest_score.y > 0.5 & temp2$highest_score.y > temp2$highest_score.x)
+
+
+venn.diagram(
+  x = list(D_ND, ND_D, unchanged),
+  category.names = c("drug -> undrug", "undrug -> drug", "unchanged"),
+  col = "transparent",  # set the color of the intersections to transparent
+  fill = c("dodgerblue", "lightcoral", "mediumseagreen"),  # set colors for each category
+  alpha = 0.5,  # set the transparency level of the circles
+  cat.col = c("dodgerblue", "lightcoral", "mediumseagreen"),  # set colors for category labels
+  cat.fontfamily = "Arial",  # set the font family for category labels
+  cat.fontface = "bold",  # set the font face for category labels
+  cat.fontsize = 10,  # set the font size for category labels
+  cex = 1.5,  # increase the size of the circles
+  margin = 0.1,  # set the margin size (proportion of the plot)
+  filename = "temp.png",
+  disable.logging = TRUE
+)
 
 
 
