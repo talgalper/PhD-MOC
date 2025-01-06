48bef41a5acf5264573a09686b097b9706d3108f
talgalper <86214510+talgalper@users.noreply.github.com>
2025-01-06 13:46:17 +1100

    Update updated_TTD_model.R

--- ML/R/updated_TTD_model.R
+++ ML/R/updated_TTD_model.R
@@ -80,9 +80,11 @@ negative_pool <- feature_matrix[!feature_matrix$Protein %in% all_target_genes, ]
 
 # Initialize parameters
 ntrees <- 1000  # Fixed number of trees
-n_models <- 100  # Number of random forests for bagging
+n_models <- 10000  # Number of random forests for bagging
 predictions <- matrix(0, nrow = nrow(feature_matrix), ncol = n_models)  # For storing predictions
-
+# for storing importance scores. remove protein and label cols
+n_features <- ncol(feature_matrix) - 3
+importance_scores <- matrix(0, nrow = n_features, ncol = n_models) 
 
 pb <- progress_bar$new(format = "[:bar] :current/:total (:percent) eta: :eta", 
                        total = n_models)
@@ -106,25 +108,39 @@ for (i in 1:n_models) {
   # Predict on the entire dataset
   predictions[, i] <- predict(rf_model, feature_matrix[, !names(feature_matrix) %in% c("approved", "Protein", "clinical")], type = "prob")[, 2]
   
-  # save at the 100th and 1000th model 
-  if (i == 100) {
-    predictions_after_100 <- predictions
-    predictions_after_100 <- predictions_after_100[1:100,1:100]
-  }
-  
-  if (i == 1000) {
-    predictions_after_1000 <- predictions
-    predictions_after_1000 <- predictions_after_1000[1:1000,1:1000]
-  }
+  # Extract feature importance 
+  importance <- importance(rf_model)
+  importance_scores[, i] <- importance[, "MeanDecreaseGini"]
   
   pb$tick()
 }
 
+save(predictions, importance_scores, file = "~/Desktop/updated_TTD_model.RData")
 
 # Average predictions across all models
 final_predictions <- rowMeans(predictions)
+
+final_predictions_after_100 <- predictions[, 1:100]
+final_predictions_after_100 <- rowMeans(final_predictions_after_100)
+
+final_predictions_after_1000 <- predictions[, 1:1000]
+final_predictions_after_1000 <- rowMeans(final_predictions_after_1000)
+
 # Attach predictions to the dataset
 feature_matrix$Prediction_Score <- final_predictions
+feature_matrix$Prediction_Score_after_100 <- final_predictions_after_100
+feature_matrix$Prediction_Score_after_1000 <- final_predictions_after_1000
+
+write.csv(feature_matrix, "results/updated_TTD_model_featureMatrix.csv")
+
+# average feature importance across all models
+importance_df <- data.frame(Features = colnames(feature_data)[!colnames(feature_data) %in% c("Label", "Protein", "validation")],
+                            Importance_100m = rowMeans(importance_scores[, 1:100]),
+                            Importance_1000m = rowMeans(importance_scores[, 1:1000]),
+                            Importance_10000m = rowMeans(importance_scores[, 1:10000]))
+
+write.csv(importance_df, "results/feature_importance/reproduced_paper_FI.csv")
+
 
 model_scores <- subset(feature_matrix, select = c("Protein", "Prediction_Score", "approved", "clinical"))
 
@@ -140,7 +156,13 @@ model_scores <- model_scores[order(-model_scores$Prediction_Score), ]
 
 # Evaluate on validation set (if provided)
 roc_curve <- roc(feature_matrix$clinical, feature_matrix$Prediction_Score)
+roc_curve_100 <- roc(feature_matrix$clinical, feature_matrix$Prediction_Score_after_100)
+roc_curve_1000 <- roc(feature_matrix$clinical, feature_matrix$Prediction_Score_after_1000)
+
 auc(roc_curve)
+auc(roc_curve_100)
+auc(roc_curve_1000)
+
 
 # Plot ROC Curve
 plot(roc_curve, main = "ROC Curve for Random Forest Predictions")
