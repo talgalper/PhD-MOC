4020ba0b4cad222854a6a15c9845e05b90934d94
talgalper <86214510+talgalper@users.noreply.github.com>
2025-01-06 13:46:14 +1100

    Update reproduced_paper.R

--- ML/R/reproduced_paper.R
+++ ML/R/reproduced_paper.R
@@ -45,7 +45,9 @@ negative_pool <- feature_data[feature_data$Label == 0 & !feature_data$Protein %i
 ntrees <- 1000  # Fixed number of trees
 n_models <- 10000  # Number of random forests for bagging
 predictions <- matrix(0, nrow = nrow(feature_data), ncol = n_models)  # For storing predictions
-
+# for storing importance scores. remove protein and label cols
+n_features <- ncol(feature_data) - 3
+importance_scores <- matrix(0, nrow = n_features, ncol = n_models) 
 
 pb <- progress_bar$new(format = "[:bar] :current/:total (:percent) eta: :eta", 
                        total = n_models)
@@ -69,9 +71,14 @@ for (i in 1:n_models) {
   # Predict on the entire dataset
   predictions[, i] <- predict(rf_model, feature_data[, !names(feature_data) %in% c("Label", "Protein", "validation")], type = "prob")[, 2]
   
+  # Extract feature importance 
+  importance <- importance(rf_model)
+  importance_scores[, i] <- importance[, "MeanDecreaseGini"]
+  
   pb$tick()
 }
 
+save(predictions, importance_scores, file = "~/Desktop/reproduced_paper_predictions.RData")
 
 # Average predictions across all models
 final_predictions <- rowMeans(predictions)
@@ -83,12 +90,22 @@ final_predictions_after_1000 <- predictions[, 1:1000]
 final_predictions_after_1000 <- rowMeans(final_predictions_after_1000)
 
 # Attach predictions to the dataset
-feature_data$Prediction_Score <- final_predictions
-feature_data$Prediction_Score_after_100 <- final_predictions_after_100
-feature_data$Prediction_Score_after_1000 <- final_predictions_after_1000
+feature_data$Prediction_Score_100m <- final_predictions_after_100
+feature_data$Prediction_Score_1000m <- final_predictions_after_1000
+feature_data$Prediction_Score_10000m <- final_predictions
+
+write.csv(feature_data, "results/reproduced_paper_featureMatrix.csv")
+
+# average feature importance across all models
+importance_df <- data.frame(Features = colnames(feature_data)[!colnames(feature_data) %in% c("Label", "Protein", "validation")],
+                            Importance_100m = rowMeans(importance_scores[, 1:100]),
+                            Importance_1000m = rowMeans(importance_scores[, 1:1000]),
+                            Importance_10000m = rowMeans(importance_scores[, 1:10000]))
+
+write.csv(importance_df, "results/feature_importance/reproduced_paper_FI.csv")
 
 
-model_scores <- subset(feature_data, select = c("Protein", "Prediction_Score", "Prediction_Score_after_100", "Prediction_Score_after_1000", "Label", "validation"))
+model_scores <- subset(feature_data, select = c("Protein", "Prediction_Score_after_100", "Prediction_Score_after_1000", "Prediction_Score_10000m", "Label", "validation"))
 
 library(biomaRt)
 ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
@@ -101,23 +118,25 @@ gene_names <- getBM(
 
 model_scores <- merge(gene_names, model_scores, by.x = "uniprotswissprot", by.y = "Protein", all.y = T)
 model_scores <- model_scores[order(-model_scores$Prediction_Score), ]
+rownames(model_scores) <- NULL
 
+write.csv(model_scores, "results/hgnc_symbol/reproduced_paper_modelScores.csv")
 
 # Evaluate on validation set (if provided)
-# validation_set <- feature_data[feature_data$validation == 1, ]  # Assuming 'Validation' column marks validation samples
+# validation_set <- feature_data[feature_data$validation == 1, ]  
 roc_curve <- roc(feature_data$validation, feature_data$Prediction_Score)
+roc_curve_100 <- roc(feature_data$validation, feature_data$Prediction_Score_after_100)
+roc_curve_1000 <- roc(feature_data$validation, feature_data$Prediction_Score_after_1000)
+
 auc(roc_curve)
+auc(roc_curve_100)
+auc(roc_curve_1000)
+
 
 # Plot ROC Curve
 plot(roc_curve, main = "ROC Curve for Random Forest Predictions")
 
-
-# Extract feature importance from one RF model as an example
-importance_scores <- importance(rf_model)
-importance_df <- data.frame(Feature = rownames(importance_scores), Importance = importance_scores[, "MeanDecreaseGini"])
-importance_df <- importance_df[order(importance_df$Importance, decreasing = TRUE), ]
-
-
+# plot importance df
 ggplot(importance_df[1:(0.5*nrow(importance_df)), ], aes(x = reorder(Feature, -Importance), y = Importance)) +
   geom_bar(stat = "identity") +
   labs(
