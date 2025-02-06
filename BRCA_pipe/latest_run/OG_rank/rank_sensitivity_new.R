7868087359d87a099db0d214630a7b0ad5e13c3e
talgalper <86214510+talgalper@users.noreply.github.com>
2025-02-06 12:06:52 +1100

    Update rank_sensitivity_new.R

--- BRCA_pipe/latest_run/OG_rank/rank_sensitivity_new.R
+++ BRCA_pipe/latest_run/OG_rank/rank_sensitivity_new.R
@@ -2,22 +2,17 @@ library(tidyverse)
 library(progress)
 library(biomaRt)
 
-ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
-
-colnames(HHnet)[5] <- "degree_centrality"
-colnames(HHnet_enrich)[5] <- "degree_centrality"
-
 # column names must match
 rank_sensitivity <- function(input_data, ensembl, topx) {
   
   # 1. Generate all weight combinations from 0..1 in steps of 0.1
   all_combos <- expand.grid(
-    betweeness_w         = seq(0, 1, 0.1),
-    citation_w           = seq(0, 1, 0.1),
-    centrality_w         = seq(0, 1, 0.1),
-    druggability_w       = seq(0, 1, 0.1),
-    eigen_centrality_w   = seq(0, 1, 0.1),
-    closeness_w          = seq(0, 1, 0.1)
+    betweeness_w = seq(0, 1, 0.1),
+    citation_w = seq(0, 1, 0.1),
+    centrality_w = seq(0, 1, 0.1),
+    druggability_w = seq(0, 1, 0.1),
+    eigen_centrality_w = seq(0, 1, 0.1),
+    closeness_w = seq(0, 1, 0.1)
   )
   
   # 2. Keep only those combos that sum to 1 (± tiny numerical tolerance)
@@ -44,12 +39,12 @@ rank_sensitivity <- function(input_data, ensembl, topx) {
     
     # Compute combined score
     combined_score <- 
-      w$betweeness_w         * input_data$betweenness +
-      w$centrality_w         * input_data$degree_centrality +
-      w$druggability_w       * input_data$highest_score +
-      w$eigen_centrality_w   * input_data$eigen_centrality +
-      w$closeness_w          * input_data$closeness -
-      w$citation_w           * input_data$counts_norm   # note the minus sign
+      w$betweeness_w * input_data$betweenness +
+      w$centrality_w * input_data$degree_centrality +
+      w$druggability_w * input_data$highest_score +
+      w$eigen_centrality_w * input_data$eigen_centrality +
+      w$closeness_w * input_data$closeness -
+      w$citation_w * input_data$counts_norm   # note the minus sign
     
     # Extract top 10 with partial sorting
     top_idx <- order(combined_score, decreasing = TRUE)[1:topx]
@@ -58,14 +53,14 @@ rank_sensitivity <- function(input_data, ensembl, topx) {
     
     # Store the result
     results_list[[i]] <- data.frame(
-      betweeness_weight       = w$betweeness_w,
-      citation_weight         = w$citation_w,
-      centrality_weight       = w$centrality_w,
-      druggability_weight     = w$druggability_w,
+      betweeness_weight = w$betweeness_w,
+      citation_weight = w$citation_w,
+      centrality_weight = w$centrality_w,
+      druggability_weight = w$druggability_w,
       eigen_centrality_weight = w$eigen_centrality_w,
-      closeness_weight        = w$closeness_w,
-      top_genes               = paste(top_genes, collapse = ', '),
-      stringsAsFactors        = FALSE
+      closeness_weight = w$closeness_w,
+      top_genes = paste(top_genes, collapse = ', '),
+      stringsAsFactors = FALSE
     )
   }
   
@@ -123,7 +118,6 @@ rank_sensitivity <- function(input_data, ensembl, topx) {
   }
   rownames(gene_counts) <- NULL
   
-  # Suppose you want to retrieve descriptions for these genes via biomaRt
   gene_description <- getBM(
     attributes = c("external_gene_name", "description"), 
     filters = "external_gene_name", 
@@ -141,6 +135,33 @@ rank_sensitivity <- function(input_data, ensembl, topx) {
   return(final_gene_counts)
 }
 
+
+
+ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
+
+HHnet <- read.csv("latest_run/OG_rank/HHnet_input.csv", row.names = 1)
+HHnet_enrich <- read.csv("latest_run/OG_rank/HHnet_enrich_input.csv", row.names = 1)
+PCSF <- read.csv("latest_run/OG_rank/PCSF_input.csv", row.names = 1)
+
+colnames(HHnet)[5] <- "degree_centrality"
+colnames(HHnet_enrich)[5] <- "degree_centrality"
+
+# normalise data
+PCSF$counts_norm <- log10(PCSF$counts)
+PCSF[, c(6:9,16)] <- lapply(PCSF[, c(6:9,16)], function(x) {
+  (x - min(x)) / (max(x) - min(x))
+})
+
+HHnet$counts_norm <- log10(HHnet$counts)
+HHnet[, c(5:8,15)] <- lapply(HHnet[, c(5:8,15)], function(x) {
+  (x - min(x)) / (max(x) - min(x))
+})
+
+HHnet_enrich$counts_norm <- log10(HHnet_enrich$counts)
+HHnet_enrich[, c(5:8,15)] <- lapply(HHnet_enrich[, c(5:8,15)], function(x) {
+  (x - min(x)) / (max(x) - min(x))
+})
+
 RS_PCSF <- rank_sensitivity(PCSF, ensembl, topx = 10)
 RS_HHnet <- rank_sensitivity(HHnet, ensembl, topx = 10)
 RS_HHnet_enrich <- rank_sensitivity(HHnet_enrich, ensembl, topx = 10)
@@ -151,5 +172,11 @@ targets <- read.csv("../Druggability_analysis/data_general/target_all_dbs.csv")
 targets <- targets[, c(2,4)]
 targets <- targets[!duplicated(targets$ensembl_gene_id), ]
 
-temp <- final_gene_counts[final_gene_counts$external_gene_name %in% targets$drugBank_target, ]
-temp <- PCSF_input[PCSF_input$external_gene_name %in% targets$drugBank_target, ]
+
+temp <- RS_HHnet_enrich[RS_HHnet_enrich$external_gene_name %in% targets$drugBank_target, ]
+temp2 <- RS_PCSF[RS_PCSF$external_gene_name %in% targets$drugBank_target, ]
+
+temp3 <- PCSF[PCSF$external_gene_name %in% targets$drugBank_target, ]
+
+
+
