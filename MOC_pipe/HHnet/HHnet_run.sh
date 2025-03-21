856684d2b924c46a0a3f21772ae80e12959fc39c
talgalper <86214510+talgalper@users.noreply.github.com>
2025-03-21 17:59:03 +1100

    Created HHnet input files

---
+++ MOC_pipe/HHnet/HHnet_run.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# IMPORTANT: Make sure to double check and update paths and filenames
+# Set the paths for your data, intermediate, and results directories
+data=$PWD/BRCA_CNV/data
+intermediate=$PWD/BRCA_CNV/intermediate
+results=$PWD/BRCA_CNV/results
+
+# Create directories
+mkdir -p $data
+mkdir -p $intermediate
+mkdir -p $results
+
+# Set the name of your network and score file
+network=STRING
+score=BRCA_CNV_freq
+
+# Filenames for your input files
+index_gene_file=gene_index.tsv
+edge_list_file=edge_list_index.tsv
+score_file=cnv_alterations.tsv
+
+num_permutations=100
+
+# This script uses gnu parallel to parallelize some of the scripts. If gnu
+# parallel is not already installed on your system, then you can install it from
+# https://www.gnu.org/software/parallel/. You can change the num_cores variable
+# to specify the number of cores for your system.
+
+##### MAKE SURE THIS IS CORRECT #####
+num_cores=32
+
+mkdir -p $intermediate/"$network"
+mkdir -p $intermediate/"$network"_"$score"
+
+# Compile Fortran module (if needed)
+#cd ../src
+#f2py -c fortran_module.f95 -m fortran_module > /dev/null
+#cd ..
+
+# Construct similarity matrix
+echo "Construct similarity matrix..."
+python src/construct_similarity_matrix.py \
+    -i   $data/"$edge_list_file" \
+    -o   $intermediate/"$network"/similarity_matrix.h5 \
+    -bof $intermediate/"$network"/beta.txt
+
+# Permute scores
+echo "Permuting scores..."
+cp $data/"$score_file" $intermediate/"$network"_"$score"/scores_0.tsv
+
+python src/find_permutation_bins.py \
+    -gsf $intermediate/"$network"_"$score"/scores_0.tsv \
+    -igf $data/"$index_gene_file" \
+    -elf $data/"$edge_list_file" \
+    -ms  1000 \
+    -o   $intermediate/"$network"_"$score"/score_bins.tsv
+
+parallel -u -j $num_cores --bar \
+    python src/permute_scores.py \
+        -i  $intermediate/"$network"_"$score"/scores_0.tsv \
+        -bf $intermediate/"$network"_"$score"/score_bins.tsv \
+        -s  {} \
+        -o  $intermediate/"$network"_"$score"/scores_{}.tsv \
+    ::: `seq $num_permutations`
+
+# Construct hierarchies
+echo "Constructing hierarchies..."
+parallel -u -j $num_cores --bar \
+    python src/construct_hierarchy.py \
+        -smf  $intermediate/"$network"/similarity_matrix.h5 \
+        -igf  $data/"$index_gene_file" \
+        -gsf  $intermediate/"$network"_"$score"/scores_{}.tsv \
+        -helf $intermediate/"$network"_"$score"/hierarchy_edge_list_{}.tsv \
+        -higf $intermediate/"$network"_"$score"/hierarchy_index_gene_{}.tsv \
+    ::: `seq 0 $num_permutations`
+
+# Process hierarchies
+echo "Processing hierarchies..."
+python src/process_hierarchies.py \
+    -oelf $intermediate/"$network"_"$score"/hierarchy_edge_list_0.tsv \
+    -oigf $intermediate/"$network"_"$score"/hierarchy_index_gene_0.tsv \
+    -pelf $(for i in `seq $num_permutations`; do echo " $intermediate/"$network"_"$score"/hierarchy_edge_list_"$i".tsv "; done) \
+    -pigf $(for i in `seq $num_permutations`; do echo " $intermediate/"$network"_"$score"/hierarchy_index_gene_"$i".tsv "; done) \
+    -lsb  10 \
+    -cf   $results/clusters_"$network"_"$score".tsv \
+    -pl   $network $score \
+    -pf   $results/sizes_"$network"_"$score".pdf \
+    -nc   $num_cores
---
+++ MOC_pipe/HHnet/file_preprocessing.R
@@ -0,0 +1,30 @@
+library(data.table)
+library(tidyverse)
+
+
+
+# load in BRCA data
+STRING_edge <- fread("../Hierarchical_HotNet/STRING_data/STRING_physical_ENSG.csv") 
+STRING_edge <- STRING_edge[, -3]
+STRING_edge <- STRING_edge[!duplicated(t(apply(STRING_edge, 1, sort))), ]
+
+# create index and indexed edge list
+gene_index <- as.data.frame(unique(c(STRING_edge$protein1_ENSG, STRING_edge$protein2_ENSG)))
+gene_index$row_num <- seq.int(nrow(gene_index))
+colnames(gene_index) <- c("ensembl_id", "index")
+gene_index <- gene_index[c("index", "ensembl_id")]
+
+edge_list_index <- data.frame(from = match(STRING_edge$protein1_ENSG, gene_index$ensembl_id),
+                              to = match(STRING_edge$protein2_ENSG, gene_index$ensembl_id))
+
+# create score file
+dif_exp <- fread("DE/MOC_DE_results.csv")
+DE_data <- subset(dif_exp, select = c("ensembl_gene_id", "logFC"))
+DE_data$logFC_abs <- abs(DE_data$logFC) # get absolute values
+DE_data_abs <- subset(DE_data, select = c("ensembl_gene_id", "logFC_abs"))
+
+fwrite(gene_index, "HHnet/data/gene_index.tsv", col.names = F, sep = "\t")
+fwrite(edge_list_index, "HHnet/data/edge_list_index.tsv", col.names = F, sep = "\t")
+fwrite(DE_data_abs, "HHnet/data/logFC_scores_abs.tsv", col.names = F, sep = "\t")
+
+
